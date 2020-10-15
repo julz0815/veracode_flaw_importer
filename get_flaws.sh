84e0f6c2d780d5fd16b51d2004c63b276c8d1520
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2020-10-15 07:55:39 +0200

    Update get_flaws.sh

--- get_flaws.sh
+++ get_flaws.sh
@@ -27,7 +27,7 @@ echo "Number of findings found: $findingsnumber"
   #Start construct SARIF
 echo "
 {
-\”\”$schema\” : \"https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json\",
+\”\$schema\” : \"https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json\",
 \"version\" : \"2.1.0\",
     \"runs\" : [
             {
