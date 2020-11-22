43c8881b0cd299b29d1455ce913fd49ce033539b
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2020-11-22 12:09:39 +0100

    Update get_flaws.sh

--- get_flaws.sh
+++ get_flaws.sh
@@ -186,13 +186,13 @@ while [  $i -lt $findingsnumber ]; do
             #if more rules add ,
             if [  $i -lt $number ]
             then
+              echo "one more rules" 
                 echo "
                 ," >> rules.json
             else
+                echo "no more rules"
                 echo "
-                    ]
-                }
-            }," >> rules.json
+                    ]," >> rules.json
             fi
 
             #Add results
@@ -262,7 +262,7 @@ done
 #create full file
 cat sarif.json > fullResults.json
 cat rules.json >> fullResults.json
-echo "      \"]results\": [" >> fullResults.json
+echo "      \"results\": [" >> fullResults.json
 cat results.json >> fullResults.json
 #close runs tag
 echo "
