b54b633255a445d2702305aebb395395778421bb
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2020-11-23 12:49:36 +0100

    Update get_flaws.sh

--- get_flaws.sh
+++ get_flaws.sh
@@ -156,8 +156,7 @@ while [  $i -lt $findingsnumber ]; do
             echo "File Name: $filename"
             pwd=$(pwd)
             echo "Folder: $pwd"
-            ls -la
-            find /home/runner/work/ -name $filename
+            find / -name $filename
 
             echo "
                     {
