61fa1ae2ef84ce9141761e168ce46b3b51c2b2e5
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2020-11-23 13:18:34 +0100

    Update get_flaws.sh

--- get_flaws.sh
+++ get_flaws.sh
@@ -158,8 +158,9 @@ while [  $i -lt $findingsnumber ]; do
             echo "Folder: $pwd"
             full_path=$(find / -name $filename)
             echo "Full Path: $full_path"
-            source_suffix=$(echo "$full_path" | sed 's/$pwd//g' | sed 's/$filepath//g')
-            echo "Source Suffix: $source_suffix"
+            source_suffix=${full_path#"$filepath"}
+            source_suffix=${source_suffix%"$pwd"}
+            echo "${source_suffix}"
 
             echo "
                     {
